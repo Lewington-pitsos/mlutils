cad1824ac5fd8522ed7c4c962f6ea00902bae966
lewington <lewington@student.unimelb.edu.au>
2019-05-30 08:41:37 +1000

    make recorder automatically sort its records before any method call that presupposes sorting

--- lepmlutils/xgbutils/recorder.py
+++ lepmlutils/xgbutils/recorder.py
@@ -10,13 +10,17 @@ class Recorder():
         self.records = sorted(self.records, key = lambda i: i["test_score"], reverse=True)
     
     def best_n_params(self, records_wanted: int) -> List[Dict]:
-        self.confirm_records()
+        self.pre_process()
         return self.records[:records_wanted]
 
     def best_params(self) -> Dict:
-        self.confirm_records()
+        self.pre_process()
         return self.records[0]
     
+    def pre_process(self):
+        self.confirm_records()
+        self.sort_records()
+
     def confirm_records(self):
         if (len(self.records) == 0):
             raise RuntimeError("Attempt to access tuning results before tuning has occurred")
--- lepmlutils/xgbutils/test_recorder.py
+++ lepmlutils/xgbutils/test_recorder.py
@@ -38,4 +38,34 @@ class TestRecorder(unittest.TestCase):
         self.assertEqual(r.best_n_params(2)[1]["test_score"],  0.8675496688741722)
         self.assertEqual(r.best_n_params(2)[1]["params"]["subsample"],  0.9600000000000004)
 
+    def test_auto_sort(self):
+        records = [
+            {'test_score': 0.8410596026490066,
+            'params': {'n_estimators': 260,
+            'subsample': 0.7800000000000002,
+            'min_split_loss': 0.75,
+            'max_depth': 8}},
+            {'test_score': 0.8675496688741722,
+            'params': {'n_estimators': 260,
+            'subsample': 0.9600000000000004,
+            'min_split_loss': 0.75,
+            'max_depth': 8}},
+            {'test_score': 0.8543046357615894,
+            'params': {'n_estimators': 260,
+            'subsample': 0.6000000000000001,
+            'min_split_loss': 0.75,
+            'max_depth': 8}},
+            {'test_score': 0.8807947019867549,
+            'params': {'n_estimators': 260,
+            'subsample': 0.9400000000000004,
+            'min_split_loss': 0.75,
+            'max_depth': 8}}
+        ]
+        
+        r = Recorder()
+        r.records = records
 
+        self.assertEqual(r.best_params()["test_score"],0.8807947019867549) 
+        self.assertEqual(r.best_params()["params"]["subsample"],0.9400000000000004) 
+        self.assertEqual(r.best_n_params(2)[1]["test_score"],  0.8675496688741722)
+        self.assertEqual(r.best_n_params(2)[1]["params"]["subsample"],  0.9600000000000004)
