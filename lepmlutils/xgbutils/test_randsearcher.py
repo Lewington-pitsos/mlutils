b0ae7fdc192e480db0248e6f68bf4e2c194f4520
lewington <lewington@student.unimelb.edu.au>
2019-05-31 18:32:46 +1000

    create RandSearcher and lightly test it

--- lepmlutils/xgbutils/test_randsearcher.py
+++ lepmlutils/xgbutils/test_randsearcher.py
@@ -1,9 +1,8 @@
 import unittest
-from typing import Type
-from .randgridsearcher import RandGridSearcher
+from .randsearcher import RandSearcher
 from .searcher import Searcher
 
-class TestRandGridSearcher(unittest.TestCase):
+class TestRandSearcher(unittest.TestCase):
     def setUp(self):
         self.params = {
             "a": [2, 4, 6],
@@ -11,7 +10,7 @@ class TestRandGridSearcher(unittest.TestCase):
         }
 
     def test_iterates_correctly(self):
-        srch: Searcher = RandGridSearcher(self.params)
+        srch: Searcher = RandSearcher(self.params)
 
         srch.__iter__()
 
@@ -20,59 +19,45 @@ class TestRandGridSearcher(unittest.TestCase):
             self.assertTrue("b" in candidates.keys())
 
     def test_iterates_exhaustively(self):
-        for _ in range(100):
-            srch = RandGridSearcher(self.params)
+        srch: Searcher = RandSearcher(self.params)
 
-            first_candidates = next(srch)
+        seen_vals = {}
 
-            self.assertGreaterEqual(3, first_candidates["a"])
-            self.assertLessEqual(2, first_candidates["a"])
-            self.assertGreaterEqual(16, first_candidates["b"])
-            self.assertLessEqual(11, first_candidates["b"])
-
-            second_candidates = next(srch)
-
-            self.assertGreaterEqual(3, second_candidates["a"])
-            self.assertLessEqual(2, second_candidates["a"])
-            self.assertGreaterEqual(27, second_candidates["b"])
-            self.assertLessEqual(17, second_candidates["b"])
-
-            for i in range(6):
-                next(srch)
-
-            last_candidates = next(srch)
-
-            self.assertGreaterEqual(6, last_candidates["a"])
-            self.assertLessEqual(5, last_candidates["a"])
-            self.assertGreaterEqual(33, last_candidates["b"])
-            self.assertLessEqual(27, last_candidates["b"])
-
-
-            self.assertRaises(StopIteration, srch.__next__)
-
-    def test_works_with_small_params(self):
-        params = {
-            "a": [2.3],
-            "b": [11.0]
-        }
+        for _ in range(60):
+            results = next(srch)
+            self.assertGreaterEqual(6, results["a"])
+            self.assertGreaterEqual(33, results["b"])
+            self.assertLessEqual(2, results["a"])
+            self.assertLessEqual(11, results["b"])
+            seen_vals[hash(frozenset(results.items()))] = True
         
-        srch: Searcher = RandGridSearcher(params)
+        self.assertEqual(9, len(seen_vals))
 
-        first_candidates = next(srch)
-
-        self.assertDictEqual({
-            "a": 2.3,
-            "b": 11.0
-        }, first_candidates)
         self.assertRaises(StopIteration, srch.__next__)
 
-    def test_works_with_lots_of_params(self):
-        self.params["c"] = [100, 101, 102, 103]
-        self.params["d"] = [100.1, 101.2, 102.3, 103.4]
-        srch: Searcher = RandGridSearcher(self.params)
-
-        count = 0
-        for _ in srch:
-            count += 1
+    # def test_works_with_small_params(self):
+    #     params = {
+    #         "a": [2.3],
+    #         "b": [11.0]
+    #     }
         
-        self.assertEqual(144, count)
+    #     srch: Searcher = RandSearcher(params)
+
+    #     first_candidates = next(srch)
+
+    #     self.assertDictEqual({
+    #         "a": 2.3,
+    #         "b": 11.0
+    #     }, first_candidates)
+    #     self.assertRaises(StopIteration, srch.__next__)
+
+    # def test_works_with_lots_of_params(self):
+    #     self.params["c"] = [100, 101, 102, 103]
+    #     self.params["d"] = [100.1, 101.2, 102.3, 103.4]
+    #     srch: Searcher = RandSearcher(self.params)
+
+    #     count = 0
+    #     for _ in srch:
+    #         count += 1
+        
+    #     self.assertEqual(144, count)
