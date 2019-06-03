2a5a95b76ff4e3da6f346203a0334a5a41925c98
lewington <lewington@student.unimelb.edu.au>
2019-06-03 13:08:55 +1000

    improve build scrpit

--- build.sh
+++ build.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+# check parameter passed in
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters: only version number required."
+    exit
+fi
+
+# Remove existing package resources.
 rm -r dist
 rm -r build
 rm -r lepmlutils.egg-info
+
+# Replace version number with argument.
+echo "$1"
+sed -i "s/version.*/version='$1',/" setup.py
+
+# Build new package resources.
 python setup.py bdist_wheel 
 python setup.py sdist
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
 
 setup(
      name='lepmlutils',  
-     version='1.82',
+     version='1.83',
      scripts=['mlutils'] ,
      author="Louka Ewington-Pitsos",
      author_email="lewington@student.unimelb.edu.au",
