b3d65ffb7fbb479dd2f9294bae068f8ad27b224a
lewington <lewington@student.unimelb.edu.au>
2019-06-03 18:23:45 +1000

    create CategorizeTfm

--- lepmlutils/pdutils/coltag.py
+++ lepmlutils/pdutils/coltag.py
@@ -3,5 +3,6 @@ from enum import Enum
 class ColTag(Enum):
     original = "original"
     modified = "modified"
+    categorized = "mapping"
     mapping = "mapping"
     bad_indicator = "bad_indicator"
