4311d87a50de3a4f1b44a845508676c6dfdf7c0d
lewington <lewington@student.unimelb.edu.au>
2019-06-14 12:21:55 +1000

    fix VotingRegressor

--- lepmlutils/pdutils/votingregressor.py
+++ lepmlutils/pdutils/votingregressor.py
@@ -10,10 +10,10 @@ class VotingRegressor(BaseEstimator, RegressorMixin, TransformerMixin):
         
     # we define clones of the original models to fit the data in
     def fit(self, X, y):
-        self.models_ = [clone(x) for x in self.models]
+        self.models = [clone(x) for x in self.models]
         
         # Train cloned base models
-        for model in self.models_:
+        for model in self.models:
             model.fit(X, y)
 
         return self
@@ -21,6 +21,6 @@ class VotingRegressor(BaseEstimator, RegressorMixin, TransformerMixin):
     #Now we do the predictions for cloned models and average them
     def predict(self, X):
         predictions = np.column_stack([
-            model.predict(X) for model in self.models_
+            model.predict(X) for model in self.models
         ]) * self.weights
         return np.sum(predictions, axis=1)   
