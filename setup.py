0b4735285fc65cb741037032ade636d42bbe0ed7
lewington <lewington@student.unimelb.edu.au>
2019-07-12 09:24:45 +1000

    upgrade downzise function

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
 
 setup(
      name='lepmlutils',  
-     version='2.1.4',
+     version='2.1.5',
      scripts=['mlutils'] ,
      author="Louka Ewington-Pitsos",
      author_email="lewington@student.unimelb.edu.au",
