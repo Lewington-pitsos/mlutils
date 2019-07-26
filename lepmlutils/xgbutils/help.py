7e7049cf0d5de285d4290880c33954849ee61129
lewington <lewington@student.unimelb.edu.au>
2019-07-26 12:04:49 +1000

    add model clear helper

--- lepmlutils/xgbutils/help.py
+++ lepmlutils/xgbutils/help.py
@@ -2,3 +2,10 @@ def best_scores(model, metric):
     res = model.evals_result()
     best = model.best_iteration
     return (res["validation_1"][metric][best], res["validation_0"][metric][best])
+
+def clear_gpu(model):
+    try:
+        model.get_booster().__del__()
+    except: 
+            pass
+    
