a3bdee22a67f36563e6c6f2e569972946c430407
lewington <lewington@student.unimelb.edu.au>
2019-07-26 11:18:34 +1000

    add best scores method to xgbutils

--- lepmlutils/xgbutils/__init__.py
+++ lepmlutils/xgbutils/__init__.py
@@ -1,6 +1,7 @@
 # This package adds helper methods to the XGBoost package. More info on this package here: https://xgboost.readthedocs.io/en/latest/
 
-from .tuner import Tuner
+from .gridsearcher import GridSearcher
+from .help import *
 from .randsearcher import RandSearcher
 from .randgridsearcher import RandGridSearcher
-from .gridsearcher import GridSearcher
+from .tuner import Tuner
