70fed34190ea0891323315f5f74f3b76fb231c00
Lewington <lewington@student.unimelb.edu.au>
2019-09-13 18:42:01 +1000

    make alter shortter

--- lepmlutils/general/audio.py
+++ lepmlutils/general/audio.py
@@ -5,7 +5,7 @@ from IPython.display import display as d
 def alert():
     """ makes sound on client using javascript (works with remote server) """      
     framerate = 44100
-    duration=1.5
+    duration=0.6
     freq=300
     t = np.linspace(0,duration,framerate*duration)
     data = np.sin(2*np.pi*freq*t)
